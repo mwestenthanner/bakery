61e289821bf0624346d040e1e9998b5ea021878f
Marianne Westenthanner <marianne@westenthanner.de>
2021-08-18 08:58:30 +0200

    Add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TeaserComponent } from './teaser/teaser.component';
+import { FiltersComponent } from './filters/filters.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TeaserComponent,
+    FiltersComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
